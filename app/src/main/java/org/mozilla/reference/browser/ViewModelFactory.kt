74c5d4d16af466d7d6deae9fb40b6fba878e0311
Stefano Pacifici <stefano.pacifici@gmail.com>
2020-05-06 21:01:14 +0200

    Restoring the ViewModelFactory (#90)
    This reverts commit f1ade73fcfa4a024759d16e86b91647d6d38992a.

---
+++ app/src/main/java/org/mozilla/reference/browser/ViewModelFactory.kt
@@ -0,0 +1,35 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.reference.browser
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.mozilla.reference.browser.library.history.ui.HistoryViewModel
+import java.lang.IllegalArgumentException
+
+/**
+ * Custom ViewModel factory that takes care of instantiating view models with non-default constructors
+ */
+class ViewModelFactory(private val applicationContext: BrowserApplication) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        when (modelClass) {
+            HistoryViewModel::class.java -> with(applicationContext.components.useCases) {
+                HistoryViewModel(historyUseCases, sessionUseCases) as T
+            }
+            else -> throw IllegalArgumentException("Unknown model class $modelClass")
+        }
+
+    companion object {
+        private var INSTANCE: ViewModelFactory? = null
+        fun getInstance(context: BrowserApplication): ViewModelFactory {
+            if (INSTANCE == null) {
+                INSTANCE = ViewModelFactory(context)
+            }
+            return INSTANCE!!
+        }
+    }
+}
